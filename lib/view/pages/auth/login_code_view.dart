0e144655e7d58b8b57755f44f67b8293df4e7336
MartaMariz <up201907020@fe.up.pt>
2022-07-14 17:54:06 +0100

    button started

--- lib/view/pages/auth/login_code_view.dart
+++ lib/view/pages/auth/login_code_view.dart
@@ -33,98 +33,125 @@ class _LoginCodePage extends State<LoginCodePage>{
     return Scaffold(
         backgroundColor: Colors.grey[200],
         body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:  [
-                //welcome
-                const Text('Esqueceu-se da password?',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25
-                    )
-                ),
-                const SizedBox(height: 15,),
-                const Text('Insira o código que lhe foi fornecido no registo para a recuperar.',
-                    style: TextStyle(
-                        fontSize: 15,
-                    ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox( height: 50,),
-
-                //code input
-                Padding( padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey[50],
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child:  Padding(
-                            padding: const EdgeInsets.only(left: 20.0),
-                            child: TextField(
-                              controller: _codeController,
-                              decoration: const InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Código'
-                              ),
-                            )
-
-                        )
-                    )
-                ),
-                const SizedBox( height: 10,),
-
-
-                //username input
-                Padding( padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey[50],
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Padding(
-                            padding: const EdgeInsets.only(left: 20.0),
-                            child: TextField(
-                              controller: _usernamaController,
-                              decoration: const InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Username'
-                              ),
-                            )
-
-                        )
-                    )
-                ),
-                const SizedBox(height: 10,),
-
-                //next button
-                Padding(padding: const EdgeInsets.symmetric(horizontal: 1.0),
-                  child: GestureDetector(
-                    onTap: resetPassword,
-                    child: Container(
-                        padding: const EdgeInsets.fromLTRB(160, 10, 160, 10),
-                        decoration:  BoxDecoration(
-                          color: mainColor,
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: const Text(
-                            'Next',
-                            style: TextStyle(
-                                color:  Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18
-                            )
-                        )
-                    ),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top : 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      MaterialButton(
+                      onPressed: () {},
+                      color: mainColor,
+                      textColor: Colors.white,
+                      child: const Icon(
+                        Icons.arrow_back,
+                        size: 24,
+                      ),
+                      padding: EdgeInsets.all(16),
+                      shape: CircleBorder(),
+                    )],
                   ),
                 ),
-                const SizedBox( height: 50,),
+                Align(
+                  alignment: Alignment.center,
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:  [
+                    //welcome
+                    const Text('Esqueceu-se da password?',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25
+                        )
+                    ),
+                    const SizedBox(height: 15,),
+                    const Padding(
+                        padding: EdgeInsets.symmetric( horizontal: 25.0),
+                        child : Text('Insira o código que lhe foi fornecido no registo para a recuperar.',
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                          textAlign: TextAlign.center,
+                        )
+                    ),
+                    const SizedBox( height: 50,),
+
+                    //code input
+                    Padding( padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[50],
+                              border: Border.all(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child:  Padding(
+                                padding: const EdgeInsets.only(left: 20.0),
+                                child: TextField(
+                                  controller: _codeController,
+                                  decoration: const InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Código'
+                                  ),
+                                )
+
+                            )
+                        )
+                    ),
+                    const SizedBox( height: 10,),
 
 
-              ],
+                    //username input
+                    Padding( padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[50],
+                              border: Border.all(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Padding(
+                                padding: const EdgeInsets.only(left: 20.0),
+                                child: TextField(
+                                  controller: _usernamaController,
+                                  decoration: const InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Username'
+                                  ),
+                                )
 
+                            )
+                        )
+                    ),
+                    const SizedBox(height: 10,),
+
+                    //next button
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 1.0),
+                      child: GestureDetector(
+                        onTap: resetPassword,
+                        child: Container(
+                            padding: const EdgeInsets.fromLTRB(160, 10, 160, 10),
+                            decoration:  BoxDecoration(
+                              color: mainColor,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: const Text(
+                                'Next',
+                                style: TextStyle(
+                                    color:  Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18
+                                )
+                            )
+                        ),
+                      ),
+                    ),
+                    const SizedBox( height: 50,),
+
+
+                  ],
+
+              ),
+                )] ,
             )
 
         )
