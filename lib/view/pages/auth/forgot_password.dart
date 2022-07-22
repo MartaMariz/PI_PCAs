c18aa524ac3e638f2f96013b87f3ddc07cf024c5
Carolina Cintra <up201906845@fe.up.pt>
2022-07-22 13:57:56 +0100

    ForgotPassword working with firebase

--- lib/view/pages/auth/forgot_password.dart
+++ lib/view/pages/auth/forgot_password.dart
@@ -4,31 +4,46 @@ import 'package:pi_pcas/view/pages/auth/auth_page_view.dart';
 import 'package:pi_pcas/view/pages/auth/reset_password_view.dart';
 import 'package:pi_pcas/view/pages/home_page_view.dart';
 
+import '../../../services/auth.dart';
 import '../../../theme.dart';
 import '../wrapper.dart';
 
-class LoginCodePage extends StatefulWidget{
-  const LoginCodePage({Key? key}): super(key: key);
+class ForgotPassword extends StatefulWidget{
+  const ForgotPassword({Key? key}): super(key: key);
 
   @override
-  _LoginCodePage createState() => _LoginCodePage();
+  _ForgotPassword createState() => _ForgotPassword();
 
 }
 
 
-class _LoginCodePage extends State<LoginCodePage>{
+class _ForgotPassword extends State<ForgotPassword>{
   //controllers
-  final _usernameController = TextEditingController();
-  final _codeController = TextEditingController();
+  final _emailController = TextEditingController();
 
+  final AuthService _auth = AuthService();
   Future resetPassword() async{
-    //usar os controllers.text e tals
-    if (mounted){
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => Wrapper(),)
+
+    dynamic result = await _auth.resetPassword(_emailController.text.trim());
+    if (result == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Email não é válido')),
       );
+      return;
+    } else {
+      print("go off sis");
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Aceda ao seu email para redefinir a password.')),
+      );
+      Navigator.pop(context);
     }
+
+  }
+
+  @override
+  void dispose(){
+    _emailController.dispose();
+    super.dispose();
   }
 
   @override
@@ -77,7 +92,7 @@ class _LoginCodePage extends State<LoginCodePage>{
                     const SizedBox(height: 15,),
                     const Padding(
                         padding: EdgeInsets.symmetric( horizontal: 25.0),
-                        child : Text('Insira o código que lhe foi fornecido no registo para a recuperar.',
+                        child : Text('Insira o email para redefinir a sua password.',
                           style: TextStyle(
                             fontSize: 15,
                           ),
@@ -86,30 +101,8 @@ class _LoginCodePage extends State<LoginCodePage>{
                     ),
                     const SizedBox( height: 50,),
 
-                    //code input
-                    Padding( padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                        child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey[50],
-                              border: Border.all(color: Colors.white),
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child:  Padding(
-                                padding: const EdgeInsets.only(left: 20.0),
-                                child: TextField(
-                                  controller: _codeController,
-                                  decoration: const InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: 'Código'
-                                  ),
-                                )
-                            )
-                        )
-                    ),
-                    const SizedBox( height: 10,),
 
-
-                    //username input
+                    //email input
                     Padding( padding: const EdgeInsets.symmetric(horizontal: 25.0),
                         child: Container(
                             decoration: BoxDecoration(
@@ -120,10 +113,10 @@ class _LoginCodePage extends State<LoginCodePage>{
                             child: Padding(
                                 padding: const EdgeInsets.only(left: 20.0),
                                 child: TextField(
-                                  controller: _usernameController,
+                                  controller: _emailController,
                                   decoration: const InputDecoration(
                                       border: InputBorder.none,
-                                      hintText: 'Username'
+                                      hintText: 'Email'
                                   ),
                                 )
 
@@ -143,7 +136,7 @@ class _LoginCodePage extends State<LoginCodePage>{
                               borderRadius: BorderRadius.circular(12),
                             ),
                             child: const Text(
-                                'Próximo',
+                                'Redefinir',
                                 style: TextStyle(
                                     color:  Colors.white,
                                     fontWeight: FontWeight.bold,
